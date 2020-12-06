ca68c9f4a79e26a35a762f488e49358528be2ab5
1315863368@163.com <1315863368@163.com>
2020-12-06 21:06:11 +0800

    优化第一版

--- springBoot-redis/src/main/java/com/itmayiedu/controller/RedisController.java
+++ springBoot-redis/src/main/java/com/itmayiedu/controller/RedisController.java
@@ -1,9 +1,6 @@
 package com.itmayiedu.controller;
 
-import com.itmayiedu.service.RedisService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 
@@ -13,32 +10,13 @@ import java.util.ArrayList;
  * @createTime: 2018/2/22
  * @version: v1.0
  */
-@RestController
+
 public class RedisController {
-    @Autowired
-    private RedisService redisService;
-
-    @RequestMapping("/getKey")
-    public String getStringKey(String key) {
-        String result = redisService.getString(key);
-        System.out.println("redis中值："+result);
-        return result;
+    public static void main(String[] args) {
+        System.out.println("测试demo2");
     }
 
-    @RequestMapping("/setKey")
-    public String setStringKey(String key, Object object) {
-        redisService.setObject(key, object);
-        return "success";
-    }
 
-    @RequestMapping("/setListKey")
-    public String setListKey() {
-        ArrayList<String> list = new ArrayList();
-        list.add("xixi");
-        list.add("niubi");
-        list.add("success");
-        redisService.setObject("list",list);
-        return "success";
-    }
+
 
 }
--- springBoot-redis/src/main/java/com/itmayiedu/service/RedisService.java
+++ springBoot-redis/src/main/java/com/itmayiedu/service/RedisService.java
@@ -1,10 +1,5 @@
 package com.itmayiedu.service;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -14,49 +9,16 @@ import java.util.concurrent.TimeUnit;
  * @createTime: 2018/2/22
  * @version: v1.0
  */
-@Service
+
 public class RedisService {
 
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-
-    public void setObject(String key, Object value) {
-        this.setObject(key, value, null);
-    }
-
-    private void setObject(String key, Object value, Long time) {
-        if (StringUtils.isEmpty(key) || value == null) {
-            return;
-        }
-        if (value instanceof String) {
-            //存放String类型
-            String stringValue = (String) value;
-            if (time == null) {
-                stringRedisTemplate.opsForValue().set(key, stringValue);
-            } else {
-                stringRedisTemplate.opsForValue().set(key, stringValue, time, TimeUnit.SECONDS);
-            }
-            return;
-        }
-        if (value instanceof List) {
-            List<String> listValue = (List<String>) value;
-            for (String string : listValue) {
-                stringRedisTemplate.opsForList().leftPush(key, string);
-            }
-        }
-    }
-
-    public void delKey(String key) {
-        stringRedisTemplate.delete(key);
-    }
-
-    public String getString(String key) {
-        return stringRedisTemplate.opsForValue().get(key);
-    }
 
     public static void main(String[] args) {
-        System.out.println("测试");
-        System.out.println("测试3");
+        System.out.println("git测试Demo2");
+
     }
+
+
+
 
 }
